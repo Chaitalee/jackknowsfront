dfd99330ed9ddd29f10cbca5965f92810e9d5b4b
Chaitalee <chaitalee.wohlig@gmail.com>
2016-06-25 19:10:47 +0530

    first-commit id this

---
+++ js/navigation.js
@@ -0,0 +1,63 @@
+// var adminurl = "http://blazen.io/";
+// var adminurl = "http://localhost:1337/";
+var adminurl = "http://chaitalee.com/";
+var imgurl = "http://146.148.4.222/upload/";
+var imgpath = adminurl + "upload/readFile";
+// var adminurl = "http://pantherworldadmin.jaipurpinkpanthers.com/";
+// var imgurl = "http://pantherworldadmin.jaipurpinkpanthers.com/upload/";
+// var imgpath = imgurl + "readFile";
+var navigationservice = angular.module('navigationservice', [])
+
+.factory('NavigationService', function($http) {
+  var navigation = [{
+    name: "Users",
+    classis: "active",
+    link: "#/page/userView",
+    subnav: []
+  }, {
+    name: "Category",
+    classis: "active",
+    link: "#/page/viewCategory",
+    subnav: []
+  }
+];
+
+  return {
+    getnav: function() {
+      return navigation;
+    },
+    makeactive: function(menuname) {
+      for (var i = 0; i < navigation.length; i++) {
+        if (navigation[i].name == menuname) {
+          navigation[i].classis = "active";
+        } else {
+          navigation[i].classis = "";
+        }
+      }
+      return menuname;
+    },
+    saveApi: function(data,apiName, successCallback, errorCallback) {
+      $http.post(adminurl + apiName, data).success(successCallback).error(errorCallback);
+    },
+    deleteProject: function(data, successCallback, errorCallback) {
+      $http.post(adminURL + "project/delete", data).success(successCallback).error(errorCallback);
+    },
+    findProjects: function(apiName,pagination, successCallback, errorCallback) {
+      $http.post(adminurl + apiName,pagination).success(successCallback).error(errorCallback);
+    },
+    findOneProject: function(apiName,urlParams, successCallback, errorCallback) {
+      console.log(adminurl+apiName);
+      $http.post(adminurl + apiName,urlParams).success(successCallback).error(errorCallback);
+    },
+    submitLogin: function(data, successCallback, errorCallback) {
+      $http.post(adminurl + "user/login", data).success(successCallback).error(errorCallback);
+    },
+    deleteApi: function(data, successCallback, errorCallback) {
+      $http.post(adminURL + "api/delete", data).success(successCallback).error(errorCallback);
+    },
+    logout: function( successCallback, errorCallback) {
+      $http.post(adminurl + "register/logout").success(successCallback).error(errorCallback);
+    },
+
+  };
+});
